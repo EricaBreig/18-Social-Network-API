29f58832397e53f1e898a0526d1e21c90f476bd1
Erica Breig <ericabreig@gmail.com>
2022-07-08 17:16:44 -0400

    added files

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -5,7 +5,7 @@ const {
   createCourse,
   updateCourse,
   deleteCourse,
-} = require('../../controllers/courseController.js');
+} = require('../../controllers/thoughts-controllers.js');
 
 // /api/courses
 router.route('/').get(getCourses).post(createCourse);
