67a01571f6f5d1354175b713030535c7cae30e46
Erica Breig <ericabreig@gmail.com>
2022-07-09 19:27:41 -0400

    updated user model again

--- models/Users.js
+++ models/Users.js
@@ -6,8 +6,9 @@ const studentSchema = new Schema(
   {
     username: {
       type: String,
+      unique: true,
       required: true,
-      max_length: 50,
+      trim: true,
     },
     email: {
       type: String,
