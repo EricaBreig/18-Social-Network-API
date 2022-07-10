88d9782caa9bc4af23612cb46285966546c7082d
Erica Breig <ericabreig@gmail.com>
2022-07-09 20:00:28 -0400

    updated data

--- utils/data.js
+++ utils/data.js
@@ -74,7 +74,7 @@ const names = [
   'Parker',
 ];
 
-const appDescriptions = [
+const appThoughts = [
   'Decision Tracker',
   'Find My Phone',
   'Learn Piano',
@@ -95,24 +95,24 @@ const appDescriptions = [
   'Deliveries',
 ];
 
-// Get a random item given an array
+// ==================== Get a random item given an array ==================== 
 const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
-// Gets a random full name
-const getRandomName = () =>
-  `${getRandomArrItem(names)} ${getRandomArrItem(names)}`;
+// ====================  Gets a random Username ==================== 
+const getRandomUsername = () =>
+  `${getRandomArrItem(usernames)} ${getRandomArrItem(usernames)}`;
 
 // Function to generate random assignments that we can add to student object.
-const getRandomAssignments = (int) => {
-  const results = [];
-  for (let i = 0; i < int; i++) {
-    results.push({
-      assignmentName: getRandomArrItem(appDescriptions),
-      score: Math.floor(Math.random() * (99 - 70 + 1) + 70),
-    });
-  }
-  return results;
-};
+// const getRandomThoughts = (int) => {
+//   const results = [];
+//   for (let i = 0; i < int; i++) {
+//     results.push({
+//       thoughtName: getRandomArrItem(appThoughts),
+//       score: Math.floor(Math.random() * (99 - 70 + 1) + 70),
+//     });
+//   }
+//   return results;
+// };
 
-// Export the functions for use in seed.js
-module.exports = { getRandomName, getRandomAssignments };
+// ==================== Export the functions for use in seed.js ==================== 
+module.exports = { getRandomName, getRandomThoughts };
