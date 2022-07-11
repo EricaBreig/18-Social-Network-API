1158a6f1893ec1a4517e4c1290859da0f441088b
Erica Breig <ericabreig@gmail.com>
2022-07-11 19:54:53 -0400

    updated user route

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -13,13 +13,12 @@ const {
 router.route("/").get(getUsers).post(createUser);
 
 // ==================== /api/users/:userId ====================
-router.route("/:userId").get(getSingleUser).put(updateSingleUser);
+router.route("/:userId").get(getSingleUser).put(updateSingleUser).delete(deleteSingleUser);
 
 // ==================== /api/users/:userId/friends/:friendId ====================
 router
   .route("/:userId/friends/:friendId")
   .post(addFriend)
-  .delete(deleteFriend)
-  .delete(deleteSingleUser);
+  .delete(deleteFriend);
 
 module.exports = router;
